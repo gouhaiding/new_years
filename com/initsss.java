356aa0007b0d4486e9cb2b7f58c10be50a932450
gouhaiding <1332487382@qq.com>
2018-01-23 17:39:10 +0800

    sss
    ssss

--- com/initsss.java
+++ com/initsss.java
@@ -1,7 +1,7 @@
 public class initsss {
     public static void main(String args[]){
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("local_gouhaiding");
     }
 
 }
